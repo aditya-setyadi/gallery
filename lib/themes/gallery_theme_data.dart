068de9f57906e7d5a5d565333a54010faffb8552
aditya.bagus@dana.id <aditya.bagus@dana.id>
2021-04-04 08:08:30 +0700

    add dependencies for test UI

--- lib/themes/gallery_theme_data.dart
+++ lib/themes/gallery_theme_data.dart
@@ -12,8 +12,7 @@ class GalleryThemeData {
   static final Color _lightFocusColor = Colors.black.withOpacity(0.12);
   static final Color _darkFocusColor = Colors.white.withOpacity(0.12);
 
-  static ThemeData lightThemeData =
-      themeData(lightColorScheme, _lightFocusColor);
+  static ThemeData lightThemeData = themeData(lightColorScheme, _lightFocusColor);
   static ThemeData darkThemeData = themeData(darkColorScheme, _darkFocusColor);
 
   static ThemeData themeData(ColorScheme colorScheme, Color focusColor) {
